765036fecaae111e4ab66e94d053a9d4a83b867e
ubednama <Shahidkhan.ak18@gmail.com>
2024-08-01 13:47:28 +0530

    feat: initial project setup
    Initial project setup for the Brainwave website.
    Includes:
     - React application structure
     - Tailwind CSS styling
     - ESLint linting
     - Vite build system
     - Necessary dependencies

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import ButtonGradient from "../public/assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+// import Button from "./components/Button"
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import RoadMap from "./components/RoadMap";
+import Services from "./components/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <RoadMap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
